82c8b387de103c52c215be1de57c892bb53e5036
TBVanderley4545 <TBVanderley@gmail.com>
2016-03-28 00:50:41 -0400

    | Added DelayedMessageService class, which extends IntentService | | Modified DelayedMessageService to remove pre-generated code, and replaced with only critical components | | Created an OnClickListener for the button created in activity_main.xml | | Ability to log the message 10 seconds after button is clicked. |

--- app/src/main/java/com/tbvanderleystudios/joke/MainActivity.java
+++ app/src/main/java/com/tbvanderleystudios/joke/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tbvanderleystudios.joke;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button delayedMessageServiceButton = (Button) findViewById(R.id.delayedMessageServiceButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DelayedMessageService.class);
+                intent.putExtra(DelayedMessageService.EXTRA_MESSAGE, getString(R.string.button_response));
+                startService(intent);
+            }
+        };
+
+        delayedMessageServiceButton.setOnClickListener(listener);
     }
 }
